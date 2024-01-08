eeb29d55e9d44fa9cdafdeefc370f6c08c6de735
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-08 14:34:08 +0530

    SSG with Dynamic Parameters Hands-on

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function Home() {
-  return <div>Next JS pre-rendering</div>;
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href="/users">
+        <h3>Users</h3>
+      </Link>
+      <Link href="/posts">
+        <h3>Posts</h3>
+      </Link>
+    </>
+  );
 }
 
 export default Home;
---
+++ src/pages/posts/[postId].js
@@ -0,0 +1,27 @@
+function Post({ post }) {
+  return (
+    <div>
+      <h2>
+        {post.id} {"  "}
+        {post.title}
+      </h2>
+      <p>{post.body}</p>
+    </div>
+  );
+}
+
+export default Post;
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
